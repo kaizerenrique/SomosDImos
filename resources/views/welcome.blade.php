86556a19d9b05b85533f851cd9969ea2486c92d4
kaizerenrique <kayserenrique@gmail.com>
2023-01-25 20:49:13 -0400

    favicon y ruta de daploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        
+        <!-- favicon -->
+        <link rel="icon" type="image/png" href="{{ url('img_plantilla/favicon.png') }}">
+
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
